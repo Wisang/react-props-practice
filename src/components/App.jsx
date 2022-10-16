522db91ee396edcaf874fecf8f3f792844d0142b
wisang <wseom7@gmail.com>
2022-10-16 22:19:35 +0900

    smaller components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      />
       <Card name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
